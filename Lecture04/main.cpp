fbdb84c97baf900ba5f42c83926eafdc4bf2db4d
seunghwan0107 <hereiam2000@naver.com>
2023-03-27 12:44:30 +0900

    *별찍기과제

---
+++ Lecture04/main.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<conio.h>
+int main()
+{
+	while(1)
+	{
+		std::cout<<"*";
+		if(kbhit())
+		{
+			std::cout<<"snffla"
+			if(getch()==27)
+				break;
+		}
+	}
+	return 0;
+}
